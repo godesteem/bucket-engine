55a0f373c796890238b6692e9a5e55d7e1776737
joshinils <joshua.laskowski@web.de>
2020-03-24 14:01:20 +0100

    refactoring, a bit of cleanup, stof -> atof

--- engine/src/engine/renderer/Mesh.cpp
+++ engine/src/engine/renderer/Mesh.cpp
@@ -12,7 +12,7 @@
 
 namespace Engine {
   Ref<Mesh> Mesh::Create(const std::string &objectFilePath, const std::string &shaderFilePath) {
-    switch(Renderer::GetAPI()){
+    switch(Renderer::GetAPI()) {
       case RendererAPI::API::None: BE_CORE_ASSERT(false, "RendererAPI::None is currently not supported ");
       case RendererAPI::API::OpenGL: return std::make_shared<OpenGLMesh>(objectFilePath, shaderFilePath);
     }
@@ -22,7 +22,7 @@ namespace Engine {
   }
 
   Ref<Mesh> Mesh::Create(Ref<VertexBuffer>& vertexBuffer, Ref<IndexBuffer>& indexBuffer, const std::string& shaderFile) {
-    switch(Renderer::GetAPI()){
+    switch(Renderer::GetAPI()) {
       case RendererAPI::API::None: BE_CORE_ASSERT(false, "RendererAPI::None is currently not supported ");
       case RendererAPI::API::OpenGL: return std::make_shared<OpenGLMesh>(vertexBuffer, indexBuffer, shaderFile);
     }
@@ -53,34 +53,40 @@ namespace Engine {
     for (size_t index = 0; index<vertexCount; ++index) {
       size_t realIndex = index * vertexElementCount;
       if (vertexCategories & VertexCategory::VertexCategoryVertex) {
-        vertexStream << "v ";
-        vertexStream << vertices[realIndex];
-        vertexStream << " ";
-        vertexStream << vertices[realIndex + 1];
-        vertexStream << " ";
-        vertexStream << vertices[realIndex + 2];
-        vertexStream << "\n";
+        vertexStream << "v "
+                     << vertices[realIndex]
+                     << ' '
+                     << vertices[realIndex + 1]
+                     << ' '
+                     << vertices[realIndex + 2]
+                     << '\n';
       }
       if (vertexCategories & VertexCategory::VertexCategoryUV) {
-        uvStream << "vt ";
-        uvStream << vertices[realIndex + 3];
-        uvStream << " ";
-        uvStream << vertices[realIndex + 4];
-        uvStream << "\n";
+        uvStream << "vt "
+                 << vertices[realIndex + 3]
+                 << ' '
+                 << vertices[realIndex + 4]
+                 << '\n';
       }
       if (vertexCategories & VertexCategory::VertexCategoryNormal) {
-        normalStream << "vn ";
-        normalStream << vertices[realIndex + 5];
-        normalStream << " ";
-        normalStream << vertices[realIndex + 6];
-        normalStream << "\n";
+        normalStream << "vn "
+                     << vertices[realIndex + 5]
+                     << ' '
+                     << vertices[realIndex + 6]
+                     << '\n';
       }
     }
     for (size_t index = 0; index<indexCount; ++index) {
-      if (index % 3 == 0) faceStream << "f ";
+      if (index % 3 == 0) {
+        faceStream << "f ";
+      }
       faceStream << index;
-      if (index % 3 == 2) faceStream << "\n";
-      else faceStream << " ";
+      if (index % 3 == 2) {
+        faceStream << "\n";
+      }
+      else {
+        faceStream << " ";
+      }
     }
     file.Write("# auto generated file\n");
     file.Write("# " + fp + "\n");
@@ -92,288 +98,241 @@ namespace Engine {
 
   bool ObjFile::LoadObjFile(std::string const& fileName)
   {
-  /* */
-    std::ifstream fin(fileName);
-
-    if (!fin)
-    {
-      BE_CORE_ASSERT(false, std::string() + __FUNCTION__ + " File " + fileName + " can not be opened.");
-      fin.close();
-      return false;
-    }
     std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
     std::vector<glm::vec3> temp_vertices;
     std::vector<glm::vec2> temp_uvs;
     std::vector<glm::vec3> temp_normals;
+    { //fin scope
+      std::ifstream fin(fileName);
 
-    while(!fin.eof())
-    {
-      std::string currentLine;
-      std::getline(fin, currentLine);
-      switch (currentLine[0])
+      if (!fin)
       {
-      case '#':
-        // comment, ignore line
-        break;
+        BE_CORE_ASSERT(false, std::string() + __FUNCTION__ + " File " + fileName + " can not be opened.");
+        fin.close();
+        return false;
+      }
 
-      case 'v':
-        // line may contain a vertex
+      while(!fin.eof())
       {
-        currentLine.erase(0, 1);
-        char vertexKind = currentLine[0];
-        if ( vertexKind != ' ' // geometric
-          && vertexKind != 't' // texture
-          && vertexKind != 'n' // normal
-          && vertexKind != 'p' // parameter space
-          )
+        std::string currentLine;
+        std::getline(fin, currentLine);
+        switch (currentLine[0])
         {
-          // unrecognized vertex specifier, ignore line
+        case '#':
+          // comment, ignore line
+          break;
+
+        case 'v':
+          // line may contain a vertex
+        {
+          currentLine.erase(0, 1);
+          char vertexKind = currentLine[0];
+          if ( vertexKind != ' ' // geometric
+            && vertexKind != 't' // texture
+            && vertexKind != 'n' // normal
+            && vertexKind != 'p' // parameter space
+            )
+          {
+            // unrecognized vertex specifier, ignore line
+            break;
+          }
+          //            currentLine.erase(0, 1);
+
+          size_t start0 = currentLine.find_first_of(' ');
+          size_t end0   = currentLine.find_first_of(' ', start0+1);
+          size_t start1 = currentLine.find_first_of(' ', end0);
+          size_t end1   = currentLine.find_first_of(' ', start1+1);
+          size_t start2 = currentLine.find_first_of(' ', end1);
+          size_t end2   = currentLine.find_first_of(' ', start2+1);
+
+          switch (vertexKind)
+          {
+          case ' ':
+          case 'n': // geometric vertex
+          {
+            float x = 0, y = 0, z = 0;
+
+            if(start0 != std::string::npos && start0 < currentLine.size())
+            try {
+              // jnl: read https://stackoverflow.com/a/57865805/10314791 atof is way faster, accepting the caveats
+              x = stof(currentLine.substr(start0, end0 - start0));
+              float foo = atof(currentLine.c_str() + start0);
+              if(x != foo) BE_CORE_ASSERT(false, std::string() + __FUNCTION__  "@" + std::to_string(__LINE__ ) + ": the values are not identical!");
+            } catch (...) { }
+
+            if (start1 != std::string::npos && start1 < currentLine.size())
+            try {
+              y = stof(currentLine.substr(start1, end1 - start1));
+              float foo = atof(currentLine.c_str() + start1);
+              if(y != foo) BE_CORE_ASSERT(false, std::string() + __FUNCTION__  "@" + std::to_string(__LINE__ ) + ": the values are not identical!");
+            }catch (...) { }
+
+            if (start2 != std::string::npos && start2 < currentLine.size())
+            try {
+              z = stof(currentLine.substr(start2, end2 - start2));
+              float foo = atof(currentLine.c_str() + start2);
+              if(z != foo) BE_CORE_ASSERT(false, std::string() + __FUNCTION__  "@" + std::to_string(__LINE__ ) + ": the values are not identical!");
+            }catch (...) { }
+
+            glm::vec3 v(x, y, z);
+            if(vertexKind == 'n') {
+              temp_normals.push_back(v);
+            } else {
+              temp_vertices.push_back(v);
+            }
+            break;
+          }
+          case 't':
+          // texture vertex
+          {
+            float x = 0, y = 0;
+
+            if(start0 != std::string::npos && start0 < currentLine.size())
+            try {
+              x = stof(currentLine.substr(start0, end0 - start0));
+            } catch (...) { }
+
+            if (start1 != std::string::npos && start1 < currentLine.size())
+            try {
+              y = stof(currentLine.substr(start1, end1 - start1));
+            }catch (...) { }
+
+            glm::vec2 v(x, y);
+            temp_uvs.push_back(v);
+            break;
+          }
+          default: // dont handle anything else
+            break;
+          }
           break;
         }
-        //            currentLine.erase(0, 1);
-
-        size_t start0 = currentLine.find_first_of(' ');
-        size_t end0   = currentLine.find_first_of(' ', start0+1);
-        size_t start1 = currentLine.find_first_of(' ', end0);
-        size_t end1   = currentLine.find_first_of(' ', start1+1);
-        size_t start2 = currentLine.find_first_of(' ', end1);
-        size_t end2   = currentLine.find_first_of(' ', start2+1);
-
-        switch (vertexKind)
+        case 'f': // face definition
         {
-        case ' ':
-        case 'n':
-          // geometric vertex
-        {
-          float x = 0, y = 0, z = 0;
+          currentLine.erase(0, 1);
+
+          size_t nSlashes = std::count(currentLine.begin(), currentLine.end(), '/');
+          size_t nDoubleSlashes = std::count(currentLine.begin(), currentLine.end(), '//');
+
+          hasUVs = nDoubleSlashes == 0 && nSlashes > 0;
+          hasNormals = nSlashes > 0;
+
+          size_t start0 = currentLine.find_first_of(' ');
+          size_t end0 = currentLine.find_first_of(' ', start0+1);
+          size_t start1 = currentLine.find_first_of(' ', end0);
+          size_t end1   = currentLine.find_first_of(' ', start1+1);
+          size_t start2 = currentLine.find_first_of(' ', end1);
+          size_t end2   = currentLine.size();
+
+          int  v1 = 0,  v2 = 0,  v3 = 0, \
+              vn1 = 0, vn2 = 0, vn3 = 0, \
+              vt1 = 0, vt2 = 0, vt3 = 0;
 
           if(start0 != std::string::npos && start0 < currentLine.size())
             try {
-            x = stof(currentLine.substr(start0, end0 - start0));
+              std::string sub = currentLine.substr(start0, end0 - start0);
+            if (!hasNormals) {
+              v1 = stoi(sub);
+            }
+            else
+            {
+              int endVal = sub.find_first_of('/');
+              std::string val = sub.substr(0, endVal);
+              v1 = stoi(val);
+              int endVal2 = sub.find_first_of('/', endVal + 1);
+              if(hasUVs) {
+                val = sub.substr(endVal + 1, endVal2);
+                vt1 = stoi(val);
+              }
+              int endVal3 = sub.find_first_of('/', endVal2 + 1);
+              val = sub.substr(endVal2 + 1, endVal3);
+              vn1 = stoi(val);
+            }
           } catch (...) { }
 
           if (start1 != std::string::npos && start1 < currentLine.size())
             try {
-            y = stof(currentLine.substr(start1, end1 - start1));
+            std::string sub = currentLine.substr(start1, end1 - start1);
+            if (!hasNormals) {
+              v2 = stoi(sub);
+            }
+            else
+            {
+              int endVal = sub.find_first_of('/');
+              std::string val = sub.substr(0, endVal);
+              v2 = stoi(val);
+              int endVal2 = sub.find_first_of('/', endVal + 1);
+              if(hasUVs) {
+                val = sub.substr(endVal + 1, endVal2);
+                vt2 = stoi(val);
+              }
+              int endVal3 = sub.find_first_of('/', endVal2 + 1);
+              val = sub.substr(endVal2 + 1, endVal3);
+              vn2 = stoi(val);
+            }
           }catch (...) { }
 
           if (start2 != std::string::npos && start2 < currentLine.size())
-            try {
-            z = stof(currentLine.substr(start2, end2 - start2));
+          try {
+            std::string sub = currentLine.substr(start2, end2 - start2);
+            if (!hasNormals) {
+              v3 = stoi(sub);
+            }
+            else {
+              int endVal = sub.find_first_of('/');
+              std::string val = sub.substr(0, endVal);
+              v3 = stoi(val);
+              int endVal2 = sub.find_first_of('/', endVal + 1);
+              if(hasUVs) {
+                val = sub.substr(endVal + 1, endVal2);
+                vt3 = stoi(val);
+              }
+              int endVal3 = sub.find_first_of('/', endVal2 + 1);
+              val = sub.substr(endVal2 + 1, endVal3);
+              vn3 = stoi(val);
+            }
           }catch (...) { }
 
-          glm::vec3 v(x, y, z);
-          if(vertexKind == 'n'){
-            temp_normals.push_back(v);
-          } else {
-            temp_vertices.push_back(v);
+          vertexIndices.push_back(v1);
+          vertexIndices.push_back(v2);
+          vertexIndices.push_back(v3);
+          if (hasUVs) {
+            uvIndices.push_back(vt1);
+            uvIndices.push_back(vt2);
+            uvIndices.push_back(vt3);
+          }
+          if (hasNormals) {
+            normalIndices.push_back(vn1);
+            normalIndices.push_back(vn2);
+            normalIndices.push_back(vn3);
           }
           break;
         }
-        case 't':
-          // texture vertex
-        {
-          float x = 0, y = 0;
-
-          if(start0 != std::string::npos && start0 < currentLine.size())
-            try {
-            x = stof(currentLine.substr(start0, end0 - start0));
-          } catch (...) { }
-
-          if (start1 != std::string::npos && start1 < currentLine.size())
-            try {
-            y = stof(currentLine.substr(start1, end1 - start1));
-          }catch (...) { }
-
-          glm::vec2 v(x, y);
-          temp_uvs.push_back(v);
+        default:
           break;
         }
-        default: // dont handle anything else
-          break;
-        }
-        break;
       }
-      case 'f': // face definition
-      {
-        currentLine.erase(0, 1);
-
-        size_t nSlashes = std::count(currentLine.begin(), currentLine.end(), '/');
-        size_t nDoubleSlashes = std::count(currentLine.begin(), currentLine.end(), '//');
-
-        hasUVs = nDoubleSlashes == 0 && nSlashes > 0;
-        hasNormals = nSlashes > 0;
-
-        size_t start0 = currentLine.find_first_of(' ');
-        size_t end0 = currentLine.find_first_of(' ', start0+1);
-        size_t start1 = currentLine.find_first_of(' ', end0);
-        size_t end1   = currentLine.find_first_of(' ', start1+1);
-        size_t start2 = currentLine.find_first_of(' ', end1);
-        size_t end2   = currentLine.size();
-
-        int  v1 = 0,  v2 = 0,  v3 = 0, \
-            vn1 = 0, vn2 = 0, vn3 = 0, \
-            vt1 = 0, vt2 = 0, vt3 = 0;
-
-        if(start0 != std::string::npos && start0 < currentLine.size())
-          try {
-            std::string sub = currentLine.substr(start0, end0 - start0);
-          if (!hasNormals)
-          {
-            v1 = stoi(sub);
-          }
-          else
-          {
-            int endVal = sub.find_first_of('/');
-            std::string val = sub.substr(0, endVal);
-            v1 = stoi(val);
-            int endVal2 = sub.find_first_of('/', endVal + 1);
-            if(hasUVs){
-              val = sub.substr(endVal + 1, endVal2);
-              vt1 = stoi(val);
-            }
-            int endVal3 = sub.find_first_of('/', endVal2 + 1);
-            val = sub.substr(endVal2 + 1, endVal3);
-            vn1 = stoi(val);
-          }
-        } catch (...) { }
-
-        if (start1 != std::string::npos && start1 < currentLine.size())
-          try {
-          std::string sub = currentLine.substr(start1, end1 - start1);
-          if (!hasNormals)
-          {
-            v2 = stoi(sub);
-          }
-          else
-          {
-            int endVal = sub.find_first_of('/');
-            std::string val = sub.substr(0, endVal);
-            v2 = stoi(val);
-            int endVal2 = sub.find_first_of('/', endVal + 1);
-            if(hasUVs){
-              val = sub.substr(endVal + 1, endVal2);
-              vt2 = stoi(val);
-            }
-            int endVal3 = sub.find_first_of('/', endVal2 + 1);
-            val = sub.substr(endVal2 + 1, endVal3);
-            vn2 = stoi(val);
-          }
-        }catch (...) { }
-
-        if (start2 != std::string::npos && start2 < currentLine.size())
-          try {
-          std::string sub = currentLine.substr(start2, end2 - start2);
-          if (!hasNormals)
-          {
-            v3 = stoi(sub);
-          }
-          else
-          {
-            int endVal = sub.find_first_of('/');
-            std::string val = sub.substr(0, endVal);
-            v3 = stoi(val);
-            int endVal2 = sub.find_first_of('/', endVal + 1);
-            if(hasUVs){
-              val = sub.substr(endVal + 1, endVal2);
-              vt3 = stoi(val);
-            }
-            int endVal3 = sub.find_first_of('/', endVal2 + 1);
-            val = sub.substr(endVal2 + 1, endVal3);
-            vn3 = stoi(val);
-          }
-        }catch (...) { }
-
-        vertexIndices.push_back(v1);
-        vertexIndices.push_back(v2);
-        vertexIndices.push_back(v3);
-        if (hasUVs) {
-          uvIndices.push_back(vt1);
-          uvIndices.push_back(vt2);
-          uvIndices.push_back(vt3);
-        }
-        if (hasNormals) {
-          normalIndices.push_back(vn1);
-          normalIndices.push_back(vn2);
-          normalIndices.push_back(vn3);
-        }
-
-        break;
-      }
-      default:
-        break;
-      }
-    }
-    fin.close();
+      fin.close();
+    } //fin scope
 
     int correction = 1;
     for (auto index : vertexIndices) {
-      if (index == 0)
-      {
+      if (index == 0) {
         correction = 0;
         break;
       }
     }
 
-    for (unsigned int i = 0; i < vertexIndices.size(); i++)
-    {
+    for (unsigned int i = 0; i < vertexIndices.size(); i++) {
       if (!temp_vertices.empty() && !vertexIndices.empty()) {
-        unsigned int vertexIndex = vertexIndices[i];
-        glm::vec3 vertex = temp_vertices[vertexIndex - correction];
-        this->vertices.push_back(vertex);
+        this->vertices.push_back(temp_vertices[vertexIndices[i] - correction]);
       }
       if (!temp_uvs.empty() && !uvIndices.empty()) {
-        unsigned int uvIndex = uvIndices[i];
-        glm::vec2 uv = temp_uvs[uvIndex - correction];
-        this->uvs.push_back(uv);
+        this->uvs.push_back(temp_uvs[uvIndices[i] - correction]);
       }
       if (!temp_normals.empty() && !normalIndices.empty()) {
-        unsigned int normalIndex = normalIndices[i];
-        glm::vec3 normal = temp_normals[normalIndex - correction];
-        this->normals.push_back(normal);
+        this->normals.push_back(temp_normals[normalIndices[i] - correction]);
       }
     }
     return true;
   }
 }
-
-#ifdef BE_PLATFORM_WINDOWS
-int getline_NERV(char ** lineptr, size_t * n, FILE * stream)
-{
-	static char line[256];
-	char *ptr;
-	unsigned int len;
-
-	if (lineptr == NULL || n == NULL)
-	{
-		errno = EINVAL;
-		return -1;
-	}
-
-	if (ferror(stream))
-		return -1;
-
-	if (feof(stream))
-		return -1;
-
-	fgets(line, 256, stream);
-
-	ptr = strchr(line, '\n');
-	if (ptr)
-		*ptr = '\0';
-
-	len = strlen(line);
-
-	if ((len + 1) < 256)
-	{
-		ptr = (char*)realloc(*lineptr, 256);
-		if (ptr == NULL)
-			return(-1);
-		*lineptr = ptr;
-		*n = 256;
-	}
-
-	strcpy(*lineptr, line);
-	return(len);
-}
-#endif
--- engine/src/engine/renderer/Mesh.h
+++ engine/src/engine/renderer/Mesh.h
@@ -18,56 +18,8 @@
 #include "OrthographicCamera.h"
 
 #ifdef BE_PLATFORM_WINDOWS
- /* Copyright (C) 1991 Free Software Foundation, Inc.
- This file is part of the GNU C Library.
-
- The GNU C Library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Library General Public License as
- published by the Free Software Foundation; either version 2 of the
- License, or (at your option) any later version.
-
- The GNU C Library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Library General Public License for more details.
-
- You should have received a copy of the GNU Library General Public
- License along with the GNU C Library; see the file COPYING.LIB.  If
- not, write to the Free Software Foundation, Inc., 675 Mass Ave,
- Cambridge, MA 02139, USA.  */
-
- /* CHANGED FOR VMS */
-
- /*
- * <getline.c>
- **
- ** HISTORY:
- **	 8 Jul 94  FM	Include "HTUtils.h" for memory allocation and free()
- **			substitutions with VAXC on VMS.
- **
- */
-
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <memory>
-
- /* Read up to (and including) a newline from STREAM into *LINEPTR
- (and null-terminate it). *LINEPTR is a pointer returned from malloc (or
- NULL), pointing to *N characters of space.  It is realloc'd as
- necessary.  Returns the number of characters read (not including the
- null terminator), or -1 on error or EOF.  */
-
-int getline_NERV(char **lineptr, size_t *n, FILE *stream);
-
-
-
-
-typedef unsigned short int ushort;
-typedef size_t ssize_t;
-#else
-#define getline_NERV getline
+  typedef unsigned short int ushort;
+  typedef size_t ssize_t;
 #endif
 
 typedef ushort GLushort;
