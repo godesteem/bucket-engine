7fa7ad2f047ce7f1f004463c1d5b1ce78d943487
Philipp Zettl <philipp@godesteem.de>
2020-04-05 12:36:16 +0200

    fix warnings

--- engine/tests/core/test_timestep.h
+++ engine/tests/core/test_timestep.h
@@ -44,7 +44,7 @@ void TestTimestep()
     Timestep t;
     t = t + 1;
     BE_TEST_MULTI(t.GetSeconds() == 1.0f);
-    BE_TEST_MULTI(t.GetMilliseconds() == 1000.0f);
+    BE_TEST_ONCE(t.GetMilliseconds() == 1000.0f);
     //Warning	C4715	'<lambda_bb557d9c8f4837619b6480b6a60b4f65>::operator()': not all control paths return a value
   });
   Tester::addTest([=]()
@@ -52,7 +52,7 @@ void TestTimestep()
     Timestep t(1.0);
     t = t - 1;
     BE_TEST_MULTI(t.GetSeconds() == 0.0f);
-    BE_TEST_MULTI(t.GetMilliseconds() == 0.0f);
+    BE_TEST_ONCE(t.GetMilliseconds() == 0.0f);
     //Warning	C4715	'<lambda_812a0c0e34e59e1725d88879a760847b>::operator()': not all control paths return a value
 
   });
@@ -74,7 +74,7 @@ void TestTimestep()
       t = t + delta;
     }
     float frameTime = 1000.f/fps;
-    BE_TEST_MULTI(t - (10.0f * frameTime) <= 0.0001);
+    BE_TEST_ONCE(t - (10.0f * frameTime) <= 0.0001);
     //Warning	C4715	'<lambda_d46812fce8d74eb6faf7b7105a2d707d>::operator()': not all control paths return a value
   });
 }
