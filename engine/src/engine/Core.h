ef4ba730ea7b5917f257a2a261f753bd4509d067
Philipp Zettl <philipp@godesteem.de>
2020-04-11 09:20:15 +0200

    move template class into namespace

--- engine/src/engine/Core.h
+++ engine/src/engine/Core.h
@@ -38,41 +38,37 @@ namespace Engine {
 #endif
 }
 
+namespace Engine {
 //jnl from http://blog.bitwigglers.org/using-enum-classes-as-type-safe-bitmasks/
 //    this enables us to use a type-safe bitmask with enum class
-template<typename Enum>
-struct EnableBitMaskOperators
-{
-  static const bool enable = false;
-};
+  template<typename Enum>
+  struct EnableBitMaskOperators {
+    static const bool enable = false;
+  };
 
-template<typename Enum>
-typename std::enable_if<EnableBitMaskOperators<Enum>::enable, Enum>::type
-operator |(Enum lhs, Enum rhs)
-{
-  using underlying = typename std::underlying_type<Enum>::type;
-  return static_cast<Enum> (
-    static_cast<underlying>(lhs) |
-    static_cast<underlying>(rhs)
+  template<typename Enum>
+  typename std::enable_if<EnableBitMaskOperators<Enum>::enable, Enum>::type
+  operator|(Enum lhs, Enum rhs) {
+    using underlying = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum> (
+        static_cast<underlying>(lhs) |
+        static_cast<underlying>(rhs)
     );
-}
+  }
 
-template<typename Enum>
-typename std::enable_if<EnableBitMaskOperators<Enum>::enable, Enum>::type
-operator &(Enum lhs, Enum rhs)
-{
-  using underlying = typename std::underlying_type<Enum>::type;
-  return static_cast<Enum> (
-    static_cast<underlying>(lhs) &
-    static_cast<underlying>(rhs)
+  template<typename Enum>
+  typename std::enable_if<EnableBitMaskOperators<Enum>::enable, Enum>::type
+  operator&(Enum lhs, Enum rhs) {
+    using underlying = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum> (
+        static_cast<underlying>(lhs) &
+        static_cast<underlying>(rhs)
     );
+  }
 }
-
 #define ENABLE_BITMASK_OPERATORS(x)  \
 template<>                           \
 struct EnableBitMaskOperators<x>     \
 {                                    \
   static const bool enable = true; \
 };
-
-
