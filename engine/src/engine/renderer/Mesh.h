89fd360a11ddabb4f7550f83bb8a0bfafdd6af04
Philipp Zettl <philipp@godesteem.de>
2020-04-13 14:56:18 +0200

    add doc comments

--- engine/src/engine/renderer/Mesh.h
+++ engine/src/engine/renderer/Mesh.h
@@ -23,15 +23,55 @@ namespace Engine
   {
   public:
     virtual ~Mesh()                    = default;
+
+    /**
+     * Enables Mesh, including activating data for rendering
+     */
     virtual void Bind() const          = 0;
+
+    /**
+     * Disables Mesh
+     */
     virtual void Unbind() const        = 0;
+
+    /**
+     * Updates and draws mesh
+     * @param ts delta since the last cycle
+     */
     virtual void OnUpdate(Timestep ts) = 0;
+
+    /**
+     * Draw function for debug interface
+     */
     virtual void OnImGuiRender()       = 0;
 
+    /**
+     * Static constructor
+     * @param objectFilePath file path to .obj file with Mesh data
+     * @param shaderFilePath file path to shader file with Shader data
+     * @return reference to Mesh object
+     */
     static Ref<Mesh> Create(const std::string& objectFilePath, const std::string& shaderFilePath);
+
+    /**
+     * Static constructor
+     * @param vertexBuffer
+     * @param indexBuffer
+     * @param shaderFile
+     * @return reference to Mesh object
+     */
     static Ref<Mesh> Create(Ref<VertexBuffer>& vertexBuffer, Ref<IndexBuffer>& indexBuffer, const std::string& shaderFile);
+
+    /**
+     * Setter for m_VertexArray.m_Size
+     * @param size
+     */
     virtual void SetVertexArraySize(uint32_t size) = 0;
+
+    /**
+     * Setter for debugging name
+     * @param name
+     */
     virtual void SetName(const std::string& name)  = 0;
-    //static Ref<Mesh> Create(const std::string& filePath);
   };
 } // namespace Engine
