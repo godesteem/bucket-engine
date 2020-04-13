423685cf4429723686d5002c2a2a106093afdce6
Philipp Zettl <philipp@godesteem.de>
2020-04-13 14:46:25 +0200

    add doc comments to vertex array

--- engine/src/engine/renderer/VertexArray.h
+++ engine/src/engine/renderer/VertexArray.h
@@ -5,26 +5,65 @@
 
 namespace Engine {
 
+  /**
+   * Stores information about vertices,
+   * this can be a combination of sorted vertex buffers
+   * or a vertex buffer in combination with a index buffer
+   */
   class VertexArray
   {
   public:
     virtual ~VertexArray() {};
 
-    virtual void Bind() const = 0;
-    virtual void Unbind() const = 0;
+    /**
+     * static constructor
+     * @return VertexArray object
+     */
+    static VertexArray* Create();
 
-    virtual void AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer) = 0;
-    virtual void SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer) = 0;
-    //virtual void SetLayout(const BufferLayout& layout) = 0;
-    //
     virtual std::vector<Ref<VertexBuffer>>& GetVertexBuffer() = 0;
+
+    /**
+     * Getter for index buffer
+     * @return attached IndexBuffer
+     */
     virtual Ref<IndexBuffer>& GetIndexBuffer() = 0;
 
-    static VertexArray* Create();
+    /**
+     * Getter for array size
+     * @return current size
+     */
     inline uint32_t GetSize() const { return m_Size; };
-    inline void SetSize(uint32_t s) { m_Size = s; };
+
+    /**
+     * Setter for IndexBuffer
+     * @param indexBuffer IndexBuffer to attach
+     */
+    virtual void SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer) = 0;
+
+    /**
+     * Setter for array size
+     * @param size
+     */
+    inline void SetSize(uint32_t size) { m_Size = size; };
+
+    /**
+     * Add a vertex buffer to the vertex array
+     * @param vertexBuffer VertexBuffer to attach
+     */
+    virtual void AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer) = 0;
+
+    /**
+     * Enables VertexArray
+     */
+    virtual void Bind() const = 0;
+
+    /**
+     * Disables VertexArray
+     */
+    virtual void Unbind() const = 0;
 
   protected:
-    uint32_t m_Size = 0;
+    uint32_t m_Size = 0; //! <Buffer size
   };
 }
